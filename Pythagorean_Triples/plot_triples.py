ef45d500c5f913fb7311d8d127f3fea9f2ef1892
Aurel Nicolae <32492300+the1howie@users.noreply.github.com>
2025-07-31 16:32:23 +0100

    Update plot_triples.py
    Signed-off-by: Aurel Nicolae <32492300+the1howie@users.noreply.github.com>

--- Pythagorean_Triples/plot_triples.py
+++ Pythagorean_Triples/plot_triples.py
@@ -5,6 +5,6 @@ import matplotlib.pyplot as plt
 sns.set_theme(style="darkgrid")
 
 headers = ["a", "b", "c"]
-df = pd.read_csv("Pythagorean_triples.csv", header=None, names=headers)
+df = pd.read_csv("pythagorean_triples.csv", header=None, names=headers)
 df.plot(x="a", y="b", style=".")
 plt.show()
